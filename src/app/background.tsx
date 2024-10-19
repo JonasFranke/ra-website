487fc44ee1198d7a3798eddd776876eadd417aa8
Jonas Franke <info@jonasfranke.xyz>
2024-10-19 12:07:23 +0200

    Run biome

--- src/app/background.tsx
+++ src/app/background.tsx
@@ -1,17 +1,17 @@
 type BackgroundProps = {
   bgImgUrl: string;
   children?: React.ReactNode;
-}
+};
 
 function BackgroundComponent({ bgImgUrl, children }: BackgroundProps) {
-
   return (
-    <div className={`relative h-screen bg-fixed bg-center bg-cover`} style={{ backgroundImage: `url(${bgImgUrl})` }}>
+    <div
+      className={`relative h-screen bg-fixed bg-center bg-cover`}
+      style={{ backgroundImage: `url(${bgImgUrl})` }}
+    >
       <div className="absolute inset-0 bg-gradient-to-t from-black/40 to-transparent"></div>
-      <div className="h-full flex items-end p-6">
-        {children}
-      </div>
-    </div >
+      <div className="h-full flex items-end p-6">{children}</div>
+    </div>
   );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import BackgroundComponent from "./background"
+import BackgroundComponent from "./background";
 import FooterComponent from "./footer";
 
 export default function HomePage() {
   return (
     <>
       <BackgroundComponent bgImgUrl="/cropped-maren-franke5.jpg">
-        <h1 className="text-white text-5xl drop-shadow-lg uppercase hyphens-manual">Raumaus&shy;stattung Franke</h1>
-      </BackgroundComponent >
+        <h1 className="text-white text-5xl drop-shadow-lg uppercase hyphens-manual">
+          Raumaus&shy;stattung Franke
+        </h1>
+      </BackgroundComponent>
       <section className="md:h-screen sm:h-min bg-white">
         <div className="md:flex md:flex-row p-10 py-20">
           <h1 className="uppercase text-gray-500 text-xl md:basis-1/2">
@@ -14,12 +16,14 @@ export default function HomePage() {
           </h1>
           <div className="md:basis-1/3">
             <p className="text-balance">
-              Herzlich Willkommen auf den Seiten der Raumausstatterin
-              Maren Franke in Bremen–Borgfeld.
+              Herzlich Willkommen auf den Seiten der Raumausstatterin Maren
+              Franke in Bremen–Borgfeld.
             </p>
             <br />
             <p className="text-balance">
-              Ich bin Maren Franke, Raumausstatterin seit über 25 Jahren,  und fertige in meinem Nähatelier an der Lilienthaler Heerstraße alles in Handarbeit.
+              Ich bin Maren Franke, Raumausstatterin seit über 25 Jahren, und
+              fertige in meinem Nähatelier an der Lilienthaler Heerstraße alles
+              in Handarbeit.
             </p>
             <br />
             <p className="text-balance">
@@ -27,23 +31,39 @@ export default function HomePage() {
             </p>
             <br />
             <p className="text-balance">
-              Ich biete Ihnen Raffrollos, Dekoschals und Stores aus ausgewählten Stoffen namhafter Hersteller und fertige nach ihren Wünschen in traditioneller handwerklicher Arbeit maßgeschneidert an.
+              Ich biete Ihnen Raffrollos, Dekoschals und Stores aus ausgewählten
+              Stoffen namhafter Hersteller und fertige nach ihren Wünschen in
+              traditioneller handwerklicher Arbeit maßgeschneidert an.
             </p>
             <br />
             <p className="text-balance">
-              Dazu besuche ich Sie gerne zuvor und berate Sie vor Ort zu gewünschten Leistungen und Designs.
+              Dazu besuche ich Sie gerne zuvor und berate Sie vor Ort zu
+              gewünschten Leistungen und Designs.
             </p>
             <br />
             <p className="text-balance">
-              Sicht- und Sonnenschutz- Anlagen, wie Plissées, Rollos, Flächenvorhänge, Vertikallamellen und Jalousien, finden Sie ebenfalls bei mir.
+              Sicht- und Sonnenschutz- Anlagen, wie Plissées, Rollos,
+              Flächenvorhänge, Vertikallamellen und Jalousien, finden Sie
+              ebenfalls bei mir.
             </p>
             <br />
             <p className="text-balance">
-              Die Montage dieser Anlagen sowie der Dekorationen mit den dazugehörenden Stilgarnituren oder Aluschienen führe ich zeitnah, sauber und  fachgerecht aus.
+              Die Montage dieser Anlagen sowie der Dekorationen mit den
+              dazugehörenden Stilgarnituren oder Aluschienen führe ich zeitnah,
+              sauber und fachgerecht aus.
             </p>
             <br />
             <p className="text-balance">
-              Ich freue mich, wenn ich Ihr Interesse an den Leistungen der Raumausstattung Franke geweckt habe. Rufen Sie mich an oder schreiben Sie mir eine <a href="mailto:info@raumausstattung-franke.com" className="underline underline-offset-1">Mail</a> mit Ihren Wünschen und Vorstellungen.
+              Ich freue mich, wenn ich Ihr Interesse an den Leistungen der
+              Raumausstattung Franke geweckt habe. Rufen Sie mich an oder
+              schreiben Sie mir eine{" "}
+              <a
+                href="mailto:info@raumausstattung-franke.com"
+                className="underline underline-offset-1"
+              >
+                Mail
+              </a>{" "}
+              mit Ihren Wünschen und Vorstellungen.
             </p>
           </div>
         </div>
@@ -54,17 +74,32 @@ export default function HomePage() {
           <div className="md:basis-1/2" />
           <div className="md:basis-1/3">
             <p className="text-balance">
-              Lesen Sie mehr über mich im <a href="http://horner-magazin.de/2019-02/" className="underline underline-offset-1">Horner Magazin</a> ab Seite 46.
+              Lesen Sie mehr über mich im{" "}
+              <a
+                href="http://horner-magazin.de/2019-02/"
+                className="underline underline-offset-1"
+              >
+                Horner Magazin
+              </a>{" "}
+              ab Seite 46.
             </p>
             <div className="md:grid md:grid-flow-row-dense md:grid-cols-2 md:items-center sm:flex sm:flex-col sm:justify-between pt-10">
-              <img src="https://www.christianfischbacher.com/ch/en/skin/frontend/digitalhome/christianfischbacher_ch_en/images/LOGO_YD_desktop.svg"
-                className="md:w-full sm:w-1/2 p-2" />
-              <img src="https://www.teba.de/images/teba_logo.svg"
-                className="md:w-full sm:w-1/2 p-2" />
-              <img src="https://www.heco-textilverlag.com/themes/Frontend/SaumViebahn/frontend/_public/src/img/logos/HECO_LOGO_dunkelgrau_bronze_dunkel.png"
-                className="md:w-full sm:w-1/2 p-2" />
-              <img src="https://www.ado-goldkante.de/typo3conf/ext/ado_goldkante/Resources/Public/Images/ado-logo.png"
-                className="md:w-full sm:w-1/2 p-2" />
+              <img
+                src="https://www.christianfischbacher.com/ch/en/skin/frontend/digitalhome/christianfischbacher_ch_en/images/LOGO_YD_desktop.svg"
+                className="md:w-full sm:w-1/2 p-2"
+              />
+              <img
+                src="https://www.teba.de/images/teba_logo.svg"
+                className="md:w-full sm:w-1/2 p-2"
+              />
+              <img
+                src="https://www.heco-textilverlag.com/themes/Frontend/SaumViebahn/frontend/_public/src/img/logos/HECO_LOGO_dunkelgrau_bronze_dunkel.png"
+                className="md:w-full sm:w-1/2 p-2"
+              />
+              <img
+                src="https://www.ado-goldkante.de/typo3conf/ext/ado_goldkante/Resources/Public/Images/ado-logo.png"
+                className="md:w-full sm:w-1/2 p-2"
+              />
             </div>
           </div>
         </div>
