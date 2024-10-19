57a636a72cfb9fdafc326acbf46ae14c4c8258bf
Jonas Franke <info@jonasfranke.xyz>
2024-10-19 11:37:22 +0200

    Convert img to next/image

--- next.config.js
+++ next.config.js
@@ -5,6 +5,31 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.heco-textilverlag.com',
+        pathname: '/themes/Frontend/SaumViebahn/frontend/_public/src/img/logos/HECO_LOGO_dunkelgrau_bronze_dunkel.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.teba.de',
+        pathname: '/images/teba_logo.svg',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.christianfischbacher.com',
+        pathname: '/ch/en/skin/frontend/digitalhome/christianfischbacher_ch_en/images/LOGO_YD_desktop.svg',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.ado-goldkante.de',
+        pathname: '/typo3conf/ext/ado_goldkante/Resources/Public/Images/ado-logo.png',
+      },
+    ]
+  }
+};
 
 export default config;
