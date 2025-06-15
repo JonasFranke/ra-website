fa5965fec0b5c9a7633351b0de0d17c6c89573a7
Jonas Franke <90050350+JonasFranke@users.noreply.github.com>
2025-06-15 13:46:05 +0200

    Add privacy page (#147)

--- src/app/image/[id]/page.tsx
+++ src/app/image/[id]/page.tsx
@@ -1,11 +1,7 @@
-"use client";
-
-import { ArrowLeft } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import { use } from "react";
+import BackButton from "~/app/components/BackButton";
 import { referenceImages } from "~/app/components/ReferenceComponent";
-import { Button } from "~/app/components/ui/button";
 
 export const runtime = "edge";
 
@@ -15,7 +11,6 @@ export default function ImagePage({
   params: Promise<{ id: string }>;
 }) {
   const { id } = use(params);
-  const router = useRouter();
 
   const image = referenceImages.find((image) => image.id === +id);
 
@@ -30,10 +25,7 @@ export default function ImagePage({
   return (
     <section className="flex flex-row md:min-h-screen sm:h-min bg-white dark:bg-slate-800 dark:text-gray-200 p-5">
       <div className="pr-5">
-        <Button onClick={() => router.push("/")} className="cursor-pointer">
-          <ArrowLeft />
-          Zurück
-        </Button>
+        <BackButton />
       </div>
       <Image
         src={image.src}
