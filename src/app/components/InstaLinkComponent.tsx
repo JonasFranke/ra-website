8038e4feff1305a322ddeb85d01a06f02cfb5078
Jonas Franke <90050350+JonasFranke@users.noreply.github.com>
2025-06-15 14:10:33 +0200

    apply biome
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/app/components/InstaLinkComponent.tsx
+++ src/app/components/InstaLinkComponent.tsx
@@ -4,7 +4,11 @@ import Link from "next/link";
 export default function InstaLinkComponent() {
   return (
     <Link href="https://instagram.com/raumausstattung.franke" target="_blank">
-      <SiInstagram title="Instagram" className="fill-black hover:fill-gray-500 dark:fill-gray-200 transition duration-100" size={32} />
+      <SiInstagram
+        title="Instagram"
+        className="fill-black hover:fill-gray-500 dark:fill-gray-200 transition duration-100"
+        size={32}
+      />
     </Link>
   );
 }
